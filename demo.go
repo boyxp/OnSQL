bf8ef8d49c0c0da7f466674c212437b1d81fa2b4
lee <isme@live.com>
2023-12-20 17:43:27 +0800

    多条分页

--- demo.go
+++ demo.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"context"
-	"encoding/json"
+	//"encoding/json"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -15,24 +15,63 @@ func main() {
 	defer Close(client)
 
 	coll := client.Database("test").Collection("test")
+/*
+	//插入单条
+	res, err := coll.InsertOne(context.TODO(), bson.M{"name":"liu","age":20})
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Println(res.InsertedID)
+*/
+/*
+	//插入多条
+	res, err := coll.InsertMany(context.TODO(), []interface{}{bson.M{"name":"liu","age":20},bson.M{"name":"ma","age":25}})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res.InsertedIDs)
+*/
+
+/*
+	//查询单条
 	filter := bson.M{"age":bson.M{"$gte":33},"name":bson.M{"$eq":"zhang"}}
 
-	var result interface{}
+	var result map[string]interface{}
 	err := coll.FindOne(context.TODO(), filter).Decode(&result)
-
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return
-		}
-		panic(err)
-	}
-
-	output, err := json.MarshalIndent(result, "", "    ")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", output)
+
+	for k, v := range result {
+		fmt.Println(k,"=",v)
+	}
+*/
+
+	//多条查询，分页、排序
+	pageSize := 2
+	pageNum := 2
+	findOptions := options.Find()
+	findOptions.SetLimit(int64(pageSize))
+	findOptions.SetSkip(int64(pageSize * (pageNum - 1)))
+	findOptions.SetSort(bson.M{"age": 1})
+
+	filter := bson.M{"age":bson.M{"$gte":1}}
+	cursor, err := coll.Find(context.TODO(), filter, findOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	var list []map[string]interface{}
+	if err = cursor.All(context.TODO(), &list); err != nil {
+		panic(err)
+	}
+
+	for _, v := range list {
+		cursor.Decode(&v)
+		fmt.Println(v)
+	}
 }
 
 func Open() *mongo.Client {
