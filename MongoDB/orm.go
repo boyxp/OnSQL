23a04324b2eff81938008cf4ba40890e8e0201c5
lee <isme@live.com>
2024-01-08 21:32:05 +0800

    结构体

--- MongoDB/orm.go
+++ MongoDB/orm.go
@@ -15,3 +15,76 @@ func (O *Orm) Init(dbtag string, table string) *Orm {
 
 	return O
 }
+
+func (O *Orm) Insert(data map[string]interface{}) string {
+	return ""
+}
+
+func (O *Orm) Delete() int64 {
+	return 1
+}
+
+func (O *Orm) Update(data map[string]interface{}) int64 {
+	return 1
+}
+
+func (O *Orm) Field(fields string) *Orm {
+	return O
+}
+
+func (O *Orm) Where(conds ...interface{}) *Orm {
+	return O
+}
+
+func (O *Orm) Group(fields ...string) *Orm {
+	return O
+}
+
+func (O *Orm) Having(field string, opr string, criteria int) *Orm {
+	return O
+}
+
+func (O *Orm) Order(field string, sort string) *Orm {
+	return O
+}
+
+func (O *Orm) Page(page int) *Orm {
+	return O
+}
+
+func (O *Orm) Limit(limit int) *Orm {
+	return O
+}
+
+func (O *Orm) Select() []map[string]interface{} {
+	return nil
+}
+
+func (O *Orm) Find() map[string]interface{} {
+	return nil
+}
+
+func (O *Orm) Value(field string) string {
+	return ""
+}
+
+func (O *Orm) Values(field string) []string {
+	return nil
+}
+
+func (O *Orm) Columns(fields ...string) map[string]string {
+	return nil
+}
+
+func (O *Orm) Sum(field string) int64 {
+	return 1
+}
+
+func (O *Orm) Count() int64 {
+	return 1
+}
+
+func (O *Orm) Exist(primary string) bool {
+	return true
+}
+
