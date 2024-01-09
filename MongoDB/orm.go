ab5a833db09d3bdb4a6f6f806888b998a50eaec6
lee <isme@live.com>
2024-01-09 12:28:28 +0800

    查询条件拼接

--- MongoDB/orm.go
+++ MongoDB/orm.go
@@ -1,12 +1,24 @@
 package MongoDB
 
 import "os"
+//import "fmt"
 import "context"
+import "strings"
 import "go.mongodb.org/mongo-driver/mongo"
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Orm struct {
 	coll *mongo.Collection
+
+	selectFields string
+	selectConds []string
+	selectParams []interface{}
+	selectPage int
+	selectLimit int
+	selectOrder []string
+	selectGroup []string
+	selectHaving string
+
 	debug string
 }
 
@@ -40,6 +52,155 @@ func (O *Orm) Field(fields string) *Orm {
 }
 
 func (O *Orm) Where(conds ...interface{}) *Orm {
+	args_len := len(conds)
+	if args_len < 1 {
+		panic("查询参数不应为空")
+	}
+
+	field, ok := conds[0].(string)
+	if !ok {
+			maps, ok := conds[0].(map[string]interface{})
+			if ok {
+				for _field,_criteria := range maps {
+						_set, ok := _criteria.([]interface{})
+						if ok {
+							_tmp := []interface{}{_field}
+							_tmp  = append(_tmp, _set...)
+							O.Where(_tmp...)
+							continue
+						}
+
+						O.Where(_field, _criteria)
+				}
+				return O
+			} else {
+				panic("第一个参数应为string类型或map[string]interface{}类型")
+			}
+	}
+
+	if placeholder_count := strings.Count(field, "?"); placeholder_count > 0 {
+		if placeholder_count != args_len -1 {
+			panic("查询占位符和参数数量不匹配")
+		}
+
+		for k,v := range conds {
+			if k==0 {
+				continue
+			}
+
+			O.selectParams = append(O.selectParams, v)
+		}
+
+		O.selectConds  = append(O.selectConds, field)
+		return O
+	}
+
+
+	switch args_len {
+		case 1 :
+				O.selectConds  = append(O.selectConds, "_id=?")
+				O.selectParams = append(O.selectParams, field)
+		case 2 :
+				O.selectConds  = append(O.selectConds, field+"=?")
+				O.selectParams = append(O.selectParams, conds[1])
+		case 3 :
+				opr, ok := conds[1].(string)
+				if !ok {
+					panic("运算符应为string类型")
+				}
+
+				opr = strings.ToTitle(opr)
+				switch opr {
+					case ">"      : fallthrough
+					case ">="     : fallthrough
+					case "<"      : fallthrough
+					case "<="     :
+									switch conds[2].(type) {
+										case int, int8, int16, int32, int64  :
+										case uint, uint8,uint16,uint32,uint64:
+										case float32,float64: 
+										default : panic(field+"参数应为数值类型")
+									}
+
+									O.selectConds  = append(O.selectConds, field+" "+opr+" ?")
+									O.selectParams = append(O.selectParams, conds[2])
+
+					case "="      : fallthrough
+					case "!="     :
+									switch conds[2].(type) {
+										case int, int8, int16, int32, int64:
+										case uint, uint8,uint16,uint32,uint64:
+										case float32,float64:
+										case string:
+										default : panic(field+"参数应为数值或字符串类型")
+									}
+
+									O.selectConds  = append(O.selectConds, field+" "+opr+" ?")
+									O.selectParams = append(O.selectParams, conds[2])
+
+					case "LIKE"   :
+									criteria, ok := conds[2].(string)
+									if !ok {
+										panic(field+"查询条件应为string类型")
+									}
+
+									O.selectConds  = append(O.selectConds, field+" "+opr+" ?")
+									O.selectParams = append(O.selectParams, criteria)
+
+					case "IN"     : fallthrough
+					case "NOT IN" :
+									criteria, ok := conds[2].([]string)
+									if !ok {
+										panic(field+"查询条件应为[]string类型")
+									}
+
+									if len(criteria)==0 {
+										criteria = append(criteria, "_in_query_placeholder_")
+									}
+
+									placeholders := []string{}
+									for _,v := range criteria {
+										placeholders   = append(placeholders, "?")
+										O.selectParams = append(O.selectParams, v)
+									}
+
+									O.selectConds  = append(O.selectConds, field+" "+opr+"("+strings.Join(placeholders, ",")+")")
+
+					case "IS"     : fallthrough
+					case "IS NOT" :
+									criteria, ok := conds[2].(string)
+									if !ok {
+										panic(field+"查询条件应为string类型")
+									}
+
+									criteria = strings.ToTitle(criteria)
+									if criteria!="NULL" {
+										panic(field+"查询条件只能为null")
+									}
+
+									O.selectConds  = append(O.selectConds, field+" "+opr+" "+criteria)
+
+					case "BETWEEN":
+									criteria, ok := conds[2].([]string)
+									if !ok {
+										panic(field+"查询条件应为[]string类型")
+									}
+
+									if len(criteria)!=2 {
+										panic(field+"查询条件应为[]string类型,且必须2个元素")
+									}
+
+									O.selectConds  = append(O.selectConds, field+" >= ? AND "+field+" <= ? ")
+									for _,v := range criteria {
+										O.selectParams = append(O.selectParams, v)
+									}
+
+					default        :
+									panic(field+"不支持的操作类型:"+opr)
+				}
+		default : panic("查询参数不应超过3个")
+	}
+
 	return O
 }
 
