8f65ff007befa1a0c67c4d5420f802bc3d48f6dd
lee <isme@live.com>
2024-01-09 09:15:39 +0800

    清除首位空格

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -1,8 +1,10 @@
 package tokenizer
 
 import "regexp"
+import "strings"
 
 func Tokenize(condition string) []string {
+	condition = strings.TrimSpace(condition)
 	condition = regexp.MustCompile(`\s*([=!><]+|\(|\)|\sand\s|\sor\s)\s*`).ReplaceAllString(condition, " $1 ")
 	condition = regexp.MustCompile(`\sin\s*\(\s*\?\s*\)`).ReplaceAllString(condition, " in ?")
 	tokens   := regexp.MustCompile(`\s+`).Split(condition, -1)
